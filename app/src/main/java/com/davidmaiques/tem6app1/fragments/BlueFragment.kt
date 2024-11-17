e0e4c358ef8fca09f95c851e0c2fa5442acaa4ac
DavidMTomas <143930697+DavidMTomas@users.noreply.github.com>
2024-11-17 21:39:10 +0100

    Initial commit

---
+++ app/src/main/java/com/davidmaiques/tem6app1/fragments/BlueFragment.kt
@@ -0,0 +1,61 @@
+package com.davidmaiques.tem6app1.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.davidmaiques.tem6app1.R
+import com.davidmaiques.tem6app1.databinding.FragmentBlueBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlueFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlueFragment : Fragment() {
+    lateinit var binding: FragmentBlueBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding= FragmentBlueBinding.inflate(inflater,container,false)
+        binding.btnFragmentAzul.setOnClickListener {
+            Toast.makeText(context, "Fragment azul presionado", Toast.LENGTH_SHORT).show()
+        }
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlueFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlueFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
